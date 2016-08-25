24c8b4390cbdeac6550a19a77bd16f40d4fb040e
David Félix-Faure <contact@felixfaure.fr>
2016-08-25 12:02:00 +0200

    Lot of js improvments

--- ressources/js/perso/main.js
+++ ressources/js/perso/main.js
@@ -54,14 +54,6 @@
 (function(window) {
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    //Helper functions
-    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    //...
-
-
-    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //Variables
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -71,29 +63,124 @@
         body = d.body,
         header = $('#header'),
         main = $('#main'),
-        windowH = w.innerHeight,
-        scrollT = w.pageYOffset;
+        windowH,
+        windowW,
+        scrollT,
+        serveur_test = d.location.hostname == "localhost";
 
 
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    //Keys event
+    //Dimensions
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // document.addEventListener('keydown', function(e) {
-    //     if(e.keyCode == 27) {
-    //         //...
-    //     }
-    // });
+    function update_dimensions() {
+      windowH = w.innerHeight;
+      windowW = w.innerWidth;
+      update_scroll();
+    }
+    function update_scroll() {
+      scrollT = w.pageYOffset;
+    }
+    update_dimensions();
+
+
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //Helper functions
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    function toCameCase(name) {
+      return name.replace(/([a-z])-([a-z])/g, function(str, m1, m2) {
+        return m1 + m2.toUpperCase();
+      }).replace(/^-/, '');
+    }
+
+    //Parse html string
+    function html2dom( htmlstr, query ) {
+      var container = d.createElement('div');
+      container.innerHTML = htmlstr;
+      return query ? $(query,container) : container.children;
+    }
+
+    //Insert after
+    function insertAfter(newNode, el) {
+      el.parentNode.insertBefore(newNode, el.nextElementSibling);
+    }
+
+    //Easing functions (inspired by http://gizma.com/easing/)
+    var easing = {
+      linear: function (t) { return t },
+      easeInQuad: function (t) { return t*t },
+      easeOutQuad: function (t) { return t*(2-t) },
+      easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+      easeInCubic: function (t) { return t*t*t },
+      easeOutCubic: function (t) { return (--t)*t*t+1 },
+      easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+      easeInQuart: function (t) { return t*t*t*t },
+      easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+      easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+      easeInQuint: function (t) { return t*t*t*t*t },
+      easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+      easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+    }
+
+    //ScrollTo animate
+    function scrollTo(Y, duration, easingFunction, callback) {
+      var start = Date.now(),
+          from = window.pageYOffset;
+
+      if(from === Y) {
+        if(callback) callback();
+        return;
+      }
+
+      if(!easingFunction) easingFunction = easing.linear;
+
+      function scroll(timestamp) {
+        var currentTime = Date.now(),
+            time = Math.min(1, ((currentTime - start) / duration)),
+            easedT = easingFunction(time);
+
+        yPos = (easedT * (Y - from)) + from;
+        window.scrollTo(0, yPos);
+
+        if(time < 1) requestAnimationFrame(scroll);
+        else if(callback) callback();
+      }
+
+      requestAnimationFrame(scroll);
+    }
+
+
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //Supports
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    if(!support.objectFit) ffclass.add(html,'no-objectfit');
+    if(!support.objectPosition) ffclass.add(html,'no-objectposition');
+
 
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //On resize
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // function onResize() {
-    //     windowH = w.innerHeight || document.documentElement.clientHeight || body.clientHeight;
-    // }
-    // window.addEventListener('resize', onResize, false);
+    function onResize() {
+      update_dimensions();
+    }
+    w.addEventListener('resize', onResize, false);
+
+
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //On scroll
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    function onScroll() {
+      update_scroll();
+    }
+    w.addEventListener('scroll', onScroll, false);
 
 })(window);
